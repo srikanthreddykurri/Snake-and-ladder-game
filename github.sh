abaf221f84dfe460f08fa0b124ef754fc2fab8f9
srikanth <srikanthreddy.k1316@gmail.com>
2025-06-07 13:37:52 -0700

    Initial commit of my game app

---
+++ github.sh
@@ -0,0 +1,8 @@
+git init
+git remote add origin https://github.com/srikanthreddykurri/Snake-and-ladder-game.git
+
+git add .
+git commit -m "Initial commit of my game app"
+git branch -M main
+git push -u origin main
+
